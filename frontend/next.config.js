d12cc5eb183eb2c3e20ce001379fa96078afa5a2
Alisson <alissonreinaldo92@gmail.com>
2022-03-11 19:55:05 -0300

    chore(frontend): :alien: Disable strict mode due to Semantic UI not being compliant to it

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // unfortunately Semantic UI is not ready for strict mode yet, we can re-enable once upgraded to Semantic UI v3
 }
 
 module.exports = nextConfig
