a06dd0452ebb330a9991a5ad31ae60f6bf2edbbd
Alisson <alissonreinaldo92@gmail.com>
2022-03-11 22:10:23 -0300

    feat(solidity): :sparkles: Added campaign's getSummary and getRequestsCount methods

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum) public {
+    function createCampaign(uint256 minimum) public {
         address newCampaign = address(new Campaign(minimum, msg.sender));
         deployedCampaigns.push(newCampaign);
     }
@@ -17,26 +17,26 @@ contract CampaignFactory {
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address recipient;
         bool complete;
-        uint approvalsCount;
+        uint256 approvalsCount;
         mapping(address => bool) approvals;
     }
 
     Request[] public requests;
-    uint requestsCount;
+    uint256 requestsCount;
     address public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public approversCount;
+    uint256 public approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    constructor(uint minimum, address creator) {
+    constructor(uint256 minimum, address creator) {
         manager = creator;
         minimumContribution = minimum;
     }
@@ -48,7 +48,11 @@ contract Campaign {
         approversCount++;
     }
 
-    function createRequest(string memory description, uint value, address recipient) public restricted {
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address recipient
+    ) public restricted {
         Request storage r = requests.push();
         r.description = description;
         r.value = value;
@@ -57,7 +61,7 @@ contract Campaign {
         r.approvalsCount = 0;
     }
 
-    function approveRequest(uint index) public {
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
 
         require(approvers[msg.sender]);
@@ -67,7 +71,7 @@ contract Campaign {
         request.approvalsCount++;
     }
 
-    function finalizeRequest(uint index) public restricted {
+    function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
 
         require(request.approvalsCount > (approversCount / 2));
@@ -76,5 +80,29 @@ contract Campaign {
         address payable recipient = payable(request.recipient); // convert to payable
         recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint256) {
+        return requests.length;
     }
 }
